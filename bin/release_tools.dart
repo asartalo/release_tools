60ef41addb3b8a32016136e7f4a5a026528c7adb
Wayne Duran <asartalo@gmail.com>
2021-03-22 10:20:24 +0800

    chore:  remove unnecessary dart files

--- bin/release_tools.dart
+++
@@ -1,5 +0,0 @@
-import 'package:release_tools/release_tools.dart' as release_tools;
-
-void main(List<String> arguments) {
-  print('Hello world: ${release_tools.calculate()}!');
-}
--- test/release_tools_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:release_tools/release_tools.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
